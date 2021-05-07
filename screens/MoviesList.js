418c384c5b722d9c201d1d6ddc9e093080fa0000
Aurélien <robineau.aure@gmail.com>
2021-05-07 14:57:45 +0200

    Add message when no movies

--- screens/MoviesList.js
+++ screens/MoviesList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, SafeAreaView } from 'react-native'
+import { StyleSheet, FlatList, SafeAreaView, Text } from 'react-native'
 
 import MovieCard from '../components/MovieCard'
 import Movie from '../models/Movie'
@@ -8,31 +8,45 @@ import OMBdAPI from '../services/OMBdAPI'
 const MoviesList = () => {
 	const [movies, setMovies] = useState([])
 
-	useEffect(() => {
-		OMBdAPI.getMovieByName('avatar')
-			.then(res => setMovies([
-				...movies,
-				new Movie(res.data.imdbID, res.data.Title, res.data.imdbRating, res.data.Poster)
-			]))
-		OMBdAPI.getMovieByName('baby_driver')
-			.then(res => setMovies([
-				...movies,
-				new Movie(res.data.imdbID, res.data.Title, res.data.imdbRating, res.data.Poster)
-			]))
-	}, [])
+	// useEffect(() => {
+	// 	OMBdAPI.getMovieByName('avatar')
+	// 		.then(res => setMovies([
+	// 			...movies,
+	// 			new Movie(res.data.imdbID, res.data.Title, res.data.imdbRating, res.data.Poster)
+	// 		]))
+	// 	OMBdAPI.getMovieByName('baby_driver')
+	// 		.then(res => setMovies([
+	// 			...movies,
+	// 			new Movie(res.data.imdbID, res.data.Title, res.data.imdbRating, res.data.Poster)
+	// 		]))
+	// }, [])
 
 	const renderMovie = ({ item }) => {
 		return <MovieCard title={item.title} rating={item.rating} />
 	}
 	return (
 		<SafeAreaView>
-			<FlatList
-				data={movies}
-				renderItem={renderMovie}
-				keyExtractor={item => item.id}
-			/>
+			{ movies.length ?
+				<FlatList
+					data={movies}
+					renderItem={renderMovie}
+					keyExtractor={item => item.id}
+				/>
+				:
+				<Text style={styles.noMovies}>
+					No movies yet...
+				</Text>
+			}
 		</SafeAreaView>
 	)
 }
+
+const styles = StyleSheet.create({
+	noMovies: {
+		fontSize: 18,
+		textAlign: 'center',
+		marginTop: 50
+	}
+})
 
 export default MoviesList
